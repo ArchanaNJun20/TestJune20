483d039f171e89006067c2aac589f962f128623d
Archana Nawale <nawalearchana0@gmail.com>
2020-08-07 20:01:33 +0530

    BranchA

--- src/P1/Test.java
+++ src/P1/Test.java
@@ -3,7 +3,7 @@ package P1;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Branch-A");
+		System.out.println("BranchA");
 		System.out.println("AAAAAAaaaaa");
 	}
 }
